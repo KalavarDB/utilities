be4db2ba91a7e5a6e715bb8792ce21e62df6cdc1
fatalcenturion <tom.b.2k2@gmail.com>
2021-01-16 02:54:26 +0000

    moved the version-utility to repository root because it makes more sense

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use tokio::*;
+use clap::{App, Arg};
+
+pub mod management;
+pub mod utilities;
+
+#[cfg(test)]
+pub mod tests;
+
+pub const VERSION: &str = "0.1.13";
+#[tokio::main]
+async fn main() {
+    let matches = App::new("Version Checker")
+        .version(VERSION)
+        .author("Thomas B. <tom.b.2k2@gmail.com>")
+        .about("Combs your Cargo.toml for dependencies, and checks their versions whilst also looking for potential security advisories")
+        .arg(Arg::with_name("manifest")
+            .short("m")
+            .long("manifest")
+            .takes_value(true)
+            .required(false)
+            .help("The path to a Cargo.toml file, if missing, the program will attempt to auto-locate the Cargo.toml")
+        )
+        .arg(Arg::with_name("no-update")
+            .short("N")
+            .long("no-update")
+            .takes_value(false)
+            .required(false)
+            .help("Disables the \"Update Available\" message until next use")
+        )
+        .arg(Arg::with_name("deep")
+            .short("d")
+            .long("deep")
+            .takes_value(false)
+            .required(false)
+            .help("Checks the dependencies of each of your dependencies, deepens search by 1 level")
+        )
+        .get_matches();
+
+    let mut recursion = 0;
+    let mut updates = true;
+
+    if matches.is_present("deep") {
+        recursion = 1;
+    }
+
+    if matches.is_present("no-update") {
+        updates = false;
+    }
+
+    let manifest = matches.value_of("manifest");
+
+    utilities::terminal::input::parse_args(manifest, recursion, updates)
+}
---
+++ src/utilities/serial/api.rs
@@ -0,0 +1,5 @@
+pub use crate_de::*;
+pub use crate_full::*;
+
+mod crate_de;
+mod crate_full;
